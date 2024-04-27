f28925779d3e55b4a2248934376eaceb16d17952
dilluti0n <hskimse1@gmail.com>
2024-04-27 19:22:06 +0900

    [suckless/dwm] Add kbd shortcuts for pavu, browser

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -73,11 +73,15 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *emacscmd[] = { "emacsclient", "-c", "-a", "\'emacs\'", NULL };
+static const char *pavucmd[] = { "pavucontrol", NULL };
+static const char *browsercmd[] = { "firefox", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+        { MODKEY|ShiftMask,             XK_p,      spawn,          {.v = pavucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+        { MODKEY,                    XK_backslash, spawn,          {.v = browsercmd } },
         { MODKEY|ShiftMask,          XK_backslash, spawn,          {.v = emacscmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
