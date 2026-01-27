31d884ff079d99d3a1a25b561cf34771c055274f
dilluti0n <hskimse1@gmail.com>
2026-01-28 08:33:56 +0900

    Use GNU stow

--- install.sh
+++ install.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-curr="$PWD"
-
-ln -sf "$curr/waybar" ~/.config/waybar
-ln -sf "$curr/sway" ~/.config/sway
-ln -sf "$curr/kanshi" ~/.config/kanshi
-ln -sf "$curr/mako" ~/.config/mako
+stow *
