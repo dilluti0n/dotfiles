d2ea351b3f39afeab23ae79f735bf1070d350809
dilluti0n <hskim@dilluti0n.com>
2026-09-21 12:29:12 +0900

    niri: seperate startup script

---
+++ niri/.config/niri/startup.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# ~/.config/niri/startup.sh
+
+dbus-update-activation-environment --systemd \
+    WAYLAND_DISPLAY DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_TYPE
+gpg-connect-agent updatestartuptty /bye
+
+# portal
+/usr/libexec/xdg-desktop-portal-gtk &
+/usr/libexec/xdg-desktop-portal -r &
+
+# session
+gentoo-pipewire-launcher &
+fcitx5 -d
+mako &
+swayosd-server &
+poweralertd &
+mute-led &
+
+# ui
+swaybg -m fit -i "$HOME/Images/wallpapers/rei.jpg" -c '#000000' &
+waybar &
+wlsunset &
+{ eww daemon && eww-subscribe-monitor; } &
+
+swayidle -w \
+    timeout 300 'swaylock -f' \
+    timeout 600 'niri msg action power-off-monitors' \
+        resume 'niri msg action power-on-monitors' \
+    before-sleep 'swaylock -f' &
+
+RUST_LOG=info chzzkd >/dev/null 2>>"$HOME/.local/state/chzzkd.log" &
